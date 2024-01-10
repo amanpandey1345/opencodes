74fb24e236ccced4a9e1efba24aca6f755a3aba9
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2024-01-10 16:02:27 +0530

    fgsdfgsfd

--- src/Components/SubNav.jsx
+++ src/Components/SubNav.jsx
@@ -10,12 +10,12 @@ const SubNav = ({ds}) => {
  
   return (
     <>
-    <div className="flex lg:w-1/2 md:w-[70%] w-[90%] h-[150px] justify-center items-center text-white">
-    <Link href={"/"}><span className={`w-[90px] h-[90px] rounded-l-2xl bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "all" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `} >ALL</span></Link>
-            <Link href={"/reel"}><span className={`w-[90px] h-[90px]  bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "reels" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}>Reels</span></Link>
-            <Link href={"/video"}><span className={`w-[90px] h-[90px]  bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "video" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}>Video</span></Link>
-            <Link href={"/photo"}><span className={`w-[90px] h-[90px]  bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "photo" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}>Photo</span></Link>
-            <Link href={"/story"}><span className={`w-[90px] h-[90px] rounded-r-2xl bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "story" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}>Story</span></Link>
+    <div className="flex lg:w-1/2 md:w-[70%] w-[90%] h-[150px] justify-center items-center text-white md:text-base text-sm">
+    <Link  href={"/"} className={`w-[90px] h-[90px] rounded-l-2xl bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "all" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}><span  >ALL</span></Link>
+            <Link href={"/reel"} className={`w-[90px] h-[90px]  bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "reels" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}><span >Reels</span></Link>
+            <Link href={"/video"} className={`w-[90px] h-[90px]  bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "video" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}><span >Video</span></Link>
+            <Link href={"/photo"} className={`w-[90px] h-[90px]  bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "photo" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}><span >Photo</span></Link>
+            <Link href={"/story"} className={`w-[90px] h-[90px] rounded-r-2xl bg-[#6228d7] flex justify-center items-center hover:scale-125 hover:bg-pink-500 hover:shadow-2xl hover:shadow-gray-700 ${ds === "story" ? "shadow-gray-700 shadow-2xl scale-125 bg-pink-500":""} `}><span >Story</span></Link>
     </div>
     </>
   )
