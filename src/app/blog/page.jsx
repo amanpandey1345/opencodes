90c6daa135685dea0bca0994bbf082daf4645ebc
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2024-01-29 01:02:43 +0530

    adfadf

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const page = async() => {
 
 
-  const res = await fetch(`${process.env.NEXTAUTH_URL}api/blog`)
+  const res = await fetch(`/api/blog`)
   const {blog} = await res.json();
     
   return ( 
